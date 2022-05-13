418ff61ede9a3d879c2107342a33c23bf539650a
oos13 <o.stevens04@gmail.com>
2022-05-13 15:34:24 -0400

    deliveries model updated, test data inserted for employee + Menu tables

--- models.py
+++ models.py
@@ -63,6 +63,7 @@ class Order(db.Model):
 
 class Deliveries(db.Model):
     delivery_no = db.Column(db.Integer, primary_key=True, nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey(Order.order_id), unique=True)
     delivery_person = db.Column(db.Integer, db.ForeignKey(Employee.emp_id), nullable=True )
     delivery_rating = db.Column(db.Integer, default=3)
     bid = db.Column(db.Integer)
