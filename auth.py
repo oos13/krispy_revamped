d79dde901e3985f51d5a73a4e4e0348dee7f9d6c
oos13 <o.stevens04@gmail.com>
2022-05-15 21:58:54 -0400

    new index page + other updates

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ from tabnanny import check
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import Employee, User
+from .models import Employee, User, Account
 from flask_login import login_required, login_user, logout_user
 
 auth = Blueprint('auth', __name__)
@@ -55,11 +55,11 @@ def signup_post():
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, address=address, password=generate_password_hash(password, method='sha256'))
-    ###new_account = Account(...)
+    new_account = Account(balance=deposit, cc_info=cc_info, user_email=email)
 
     # add the new user to the database
     db.session.add(new_user)
-    ###db.session.add(new_account)
+    db.session.add(new_account)
     db.session.commit()
 
     return redirect(url_for('auth.login'))
