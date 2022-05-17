efdf0a012253fff48543f42e477d414c1901649d
oos13 <o.stevens04@gmail.com>
2022-05-16 23:36:42 -0400

    round 2

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
-    login_manager.login_view = 'auth.login_employee'
+    login_manager.login_view = 'auth.employee_login'
     login_manager.init_app(app)
 
     from .models import User, Employee
